2feaff21f1abae45b6ecb6f8db4a02482d90c448
dkvir <kvirika137@gmail.com>
2023-12-31 00:15:48 +0400

    👌 IMPROVE: building

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clavius-adventures'`, () => {
+  it(`should have as title 'Angular-lucky-wheel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clavius-adventures');
+    expect(app.title).toEqual('Angular-lucky-wheel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clavius-adventures app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Angular-lucky-wheel app is running!'
+    );
   });
 });
